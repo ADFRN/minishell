b2eb50a73cd878221a2ba1310174441dfda51e73
Tuan Tiprez <tipr.tuan@gmail.com>
2026-04-07 14:08:44 +0200

    mise en commentaire du main de pipex

--- src/main/main.c
+++ src/main/main.c
@@ -6,7 +6,7 @@
 /*   By: ttiprez <ttiprez@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/12 13:48:40 by ttiprez           #+#    #+#             */
-/*   Updated: 2026/04/07 12:02:05 by ttiprez          ###   ########.fr       */
+/*   Updated: 2026/04/07 14:08:24 by ttiprez          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -76,21 +76,21 @@ int	main(int ac, char **av, char **envp)
 	return (EXIT_SUCCESS);
 }
 
-int	main(int ac, char **av, char **envp)
-{
-	t_lstcmd	*lstcmd;
-	int			fd_in;
-	int			fd_out;
-	int			exit_status;
+//int	main(int ac, char **av, char **envp)
+//{
+//	t_lstcmd	*lstcmd;
+//	int			fd_in;
+//	int			fd_out;
+//	int			exit_status;
 
-	argv_checker(ac, av);
-	fd_out = open_output_file(av[ac - 1], is_heredoc(av));
-	fd_in = open_input_file(av[1], av);
-	if (fd_in == -2)
-		return (cleanup(NULL, fd_in, fd_out), EXIT_FAILURE);
-	lstcmd = init_lstcmd(is_heredoc(av), ac, av, envp);
-	if (!lstcmd)
-		return (cleanup(NULL, fd_in, fd_out), EXIT_FAILURE);
-	exit_status = execute_pipeline(lstcmd, fd_in, fd_out);
-	return (cleanup(lstcmd, fd_in, fd_out), exit_status);
-}
+//	argv_checker(ac, av);
+//	fd_out = open_output_file(av[ac - 1], is_heredoc(av));
+//	fd_in = open_input_file(av[1], av);
+//	if (fd_in == -2)
+//		return (cleanup(NULL, fd_in, fd_out), EXIT_FAILURE);
+//	lstcmd = init_lstcmd(is_heredoc(av), ac, av, envp);
+//	if (!lstcmd)
+//		return (cleanup(NULL, fd_in, fd_out), EXIT_FAILURE);
+//	exit_status = execute_pipeline(lstcmd, fd_in, fd_out);
+//	return (cleanup(lstcmd, fd_in, fd_out), exit_status);
+//}
