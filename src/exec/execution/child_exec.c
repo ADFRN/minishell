d111f2b24162bb1c0b102c61c1913140f8326bf3
Tuan Tiprez <tipr.tuan@gmail.com>
2026-04-08 15:30:21 +0200

    Fini

--- src/exec/execution/child_exec.c
+++ src/exec/execution/child_exec.c
@@ -6,13 +6,13 @@
 /*   By: ttiprez <ttiprez@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/10 19:50:19 by ttiprez           #+#    #+#             */
-/*   Updated: 2026/04/07 15:48:51 by ttiprez          ###   ########.fr       */
+/*   Updated: 2026/04/08 14:02:42 by ttiprez          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	cmd_not_found(char *cmd)
+																																					void	cmd_not_found(char *cmd)
 {
 	ft_putstr_fd(cmd, 2);
 	ft_putendl_fd(": command not found", 2);
@@ -27,19 +27,19 @@ void	close_all_fd(void)
 		close(fd++);
 }
 
-//static void	exec_command(t_lstcmd *lst, t_lstcmd *cmd)
-//{
-//	if (!cmd->cmd_with_path)
-//	{
-//		cmd_not_found(cmd->cmd_split[0]);
-//		free_lstcmd(lst);
-//		exit(CMD_NOT_FOUND);
-//	}
-//	execve(cmd->cmd_with_path, cmd->cmd_split, cmd->envp);
-//	perror(cmd->cmd_split[0]);
-//	free_lstcmd(lst);
-//	return (exit(CMD_EXEC_ERROR));
-//}
+										//static void	exec_command(t_lstcmd *lst, t_lstcmd *cmd)
+										//{
+										//	if (!cmd->cmd_with_path)
+										//	{
+										//		cmd_not_found(cmd->cmd_split[0]);
+										//		free_lstcmd(lst);
+										//		exit(CMD_NOT_FOUND);
+										//	}
+										//	execve(cmd->cmd_with_path, cmd->cmd_split, cmd->envp);
+										//	perror(cmd->cmd_split[0]);
+										//	free_lstcmd(lst);
+										//	return (exit(CMD_EXEC_ERROR));
+										//}
 
 //int	child_action(t_lstcmd *lst, t_lstcmd *cmd, int from, int to)
 //{
--- src/main/main.c
+++ src/main/main.c
@@ -3,16 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: afournie <afournie@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ttiprez <ttiprez@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/12 13:48:40 by ttiprez           #+#    #+#             */
-/*   Updated: 2026/04/08 12:12:56 by afournie         ###   ########.fr       */
+/*   Updated: 2026/04/08 15:19:04 by ttiprez          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static void	exec_cmd(t_cmd *cmd, char **envp)
+static void exec_cmd(t_cmd *cmd, char **envp)
 {
 	if (!get_cmd_with_path(cmd->args[0], get_path(envp)))
 	{
@@ -23,55 +23,84 @@ static void	exec_cmd(t_cmd *cmd, char **envp)
 	perror(cmd->args[0]);
 }
 
-static int	child_action(t_cmd *cmd, int from, char **envp)
+static int child_action(t_cmd *cmd, int from, int to, char **envp)
 {
-	pid_t	child;
+	pid_t child;
 
 	if (from < 0)
+	{
+		if (to >= 0)
+			close(to);
 		return (0);
+	}
 	child = fork();
 	if (child == -1)
-		exit((printf("fail\n"), EXIT_FAILURE)); // fail
+		exit((printf("fail\n"), EXIT_FAILURE)); // TODO:sortir proprement
 	if (child == 0)
 	{
 		if (dup2(from, STDIN_FILENO) == -1)
-			return (1);
-		close(from);
+			return (1); // TODO : sortir proprement
+		if (dup2(to, STDOUT_FILENO) == -1)
+			return (1); // TODO : sortir proprement
 		close_all_fd();
 		exec_cmd(cmd, envp);
 	}
-	return (0);
+	return (0); // TODO: sortir proprement
 }
 
-static int	pipex(t_cmd **lst_cmd, char **envp)
+static int pipex(t_cmd **lst_cmd, char **envp)
 {
-	t_cmd	*current;
-	int		input_fd;
-	int		last_pid;
+	t_cmd *current;
+	int pipe_fd[2];
+	int input_fd;
+	int output_fd;
+	int last_pid;
 
 	current = *lst_cmd;
+	input_fd = STDIN_FILENO;
 	last_pid = 0;
 	while (current)
 	{
-		if (current->redir_in)
+		output_fd = STDOUT_FILENO;
+		if (current->next)
 		{
+			if (pipe(pipe_fd) == -1)
+				return (perror("pipe"), -1); // TODO : Gerer ereur
+			output_fd = pipe_fd[1];
+		}
+		if (current->redir_out)
+		{
+			if (output_fd != STDOUT_FILENO)
+				close(output_fd);
+			if (current->append)
+				output_fd = open(current->redir_out,
+								 O_WRONLY | O_CREAT | O_APPEND, 0644);
+			else
+				output_fd = open(current->redir_out,
+								 O_WRONLY | O_CREAT | O_TRUNC, 0644);
+		}
+		if (current->redir_in) // TODO : Gerer heredoc
 			input_fd = open(current->redir_in, O_RDONLY);
-			// GERER LE FAIL
-		}
-		if (current->args[0])
-		{
-			last_pid = child_action(current, input_fd, envp);
-		}
+		if (current->args)
+			last_pid = child_action(current, input_fd, output_fd, envp);
+		if (input_fd != STDIN_FILENO)
+			close(input_fd);
+		if (output_fd != STDOUT_FILENO)
+			close(output_fd);
+		if (current->next)
+			input_fd = pipe_fd[0];
+		else
+			input_fd = STDIN_FILENO;
 		current = current->next;
 	}
 	return (wait_for_children(last_pid));
 }
 
-static void	shell_prompt(char **envcpy)
+static void shell_prompt(char **envcpy)
 {
-	char	*rl;
-	t_token	*lst_token;
-	t_cmd	*lst_cmd;
+	char *rl;
+	t_token *lst_token;
+	t_cmd *lst_cmd;
 
 	lst_token = NULL;
 	// MINISHELL
@@ -79,16 +108,15 @@ static void	shell_prompt(char **envcpy)
 	{
 		// READLINE
 		rl = readline("Minishell-1.0$ ");
-		printf("rl : %s \n", rl);
 		if (!rl)
 		{
 			printf("exit\n");
-			break ;
+			break;
 		}
 		if (rl[0] == '\0')
 		{
 			free(rl);
-			continue ;
+			continue;
 		}
 		add_history(rl);
 		// VERIFICATION READLINE
@@ -96,19 +124,18 @@ static void	shell_prompt(char **envcpy)
 		{
 			printf("error: unclosed quote\n");
 			free(rl);
-			continue ;
+			continue;
 		}
 		// LEXER
 		expand(&rl, envcpy);
-		printf("\nrl expand : %s\n\n", rl);
 		lst_token = tokenizer(rl);
 		if (!lst_token)
-			continue ;
+			continue;
 		// AFFICHAGE
 		lst_cmd = parser(&lst_token);
+		ft_print_lst_cmd(&lst_cmd);
 		pipex(&lst_cmd, envcpy);
 		ft_token_clear(&lst_token);
-		ft_print_lst_cmd(&lst_cmd);
 		// FREE
 		ft_free();
 	}
@@ -116,9 +143,9 @@ static void	shell_prompt(char **envcpy)
 	ft_free();
 }
 
-int	main(int ac, char **av, char **envp)
+int main(int ac, char **av, char **envp)
 {
-	char	**envcpy;
+	char **envcpy;
 
 	(void)ac;
 	(void)av;
