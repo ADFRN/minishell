2a7be878bf079de3cf2c762cf6cc3551780c4244
Tuan Tiprez <tipr.tuan@gmail.com>
2026-04-17 13:59:02 +0200

    exec_builtins

--- src/exec/execution/builtins_exec.c
+++ src/exec/execution/builtins_exec.c
@@ -6,20 +6,14 @@
 /*   By: ttiprez <ttiprez@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/17 13:35:05 by ttiprez           #+#    #+#             */
-/*   Updated: 2026/04/17 13:54:28 by ttiprez          ###   ########.fr       */
+/*   Updated: 2026/04/17 13:58:41 by ttiprez          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	exec_bui(t_cmd *cmd)
+void	exec_builtins(t_cmd *cmd)
 {
-	if (!cmd->cmd_with_path)
-	{
-		cmd_not_found(cmd->args[0]);
-		exit((free_env(cmd->envp), ft_free(), EXIT_FAILURE));
-	}
-	execve(cmd->cmd_with_path, cmd->args, cmd->envp);
-	perror(cmd->args[0]);
-	exit((free_env(cmd->envp), ft_free(), EXIT_FAILURE));
+	(void)cmd;
+	printf("BUILTINS\n");
 }
