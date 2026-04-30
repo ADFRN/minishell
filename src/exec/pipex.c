b84c68dabc6e698250827f12844969679f0a9c50
afournie <adrien.fournier1998@hotmail.fr>
2026-04-30 17:03:54 +0200

    ntm

--- src/exec/pipex.c
+++ src/exec/pipex.c
@@ -6,7 +6,7 @@
 /*   By: afournie <afournie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/09 11:58:18 by ttiprez           #+#    #+#             */
-/*   Updated: 2026/04/30 15:23:45 by afournie         ###   ########.fr       */
+/*   Updated: 2026/04/30 17:03:43 by afournie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,13 +39,10 @@ int	pipex(t_mini *mini)
 		return (exec_solo_builtin(mini, c_cmds));
 	while (c_cmds)
 	{
-		pipe_fd[0] = -1;
-		pipe_fd[1] = -1;
+		free((pipe_fd[0] = -1, pipe_fd[1] = -1, NULL));
 		if (c_cmds->next && pipe(pipe_fd) == -1)
 			exit((perror("pipe"), cleaning(&mini->env), EXIT_FAILURE));
 		last_pid = child_action(mini, c_cmds, input_fd, pipe_fd);
-		close(input_fd);
-		close(pipe_fd[1]);
 		input_fd = pipe_fd[0];
 		c_cmds = c_cmds->next;
 	}
